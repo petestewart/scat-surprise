cf37e3d32573cf2abf883d6127c08a74984ef1c6
Pete Stewart <petestew@gmail.com>
2020-08-29 10:02:18 -0500

    auth completed

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,41 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import Auth from '../components/pages/Auth/Auth';
+import Navbar from '../components/pages/Navbar/Navbar';
+
+import fbConnection from '../helpers/data/connection';
+
 import './App.scss';
 
+fbConnection();
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
     return (
       <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info">I am a button</button>
+        <h2>BIRB WATCHER</h2>
+        <Navbar />
+        <Auth />
       </div>
     );
   }
