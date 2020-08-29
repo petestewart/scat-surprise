e13471995aae7acad64d432118d949e8ab6ded29
Pete Stewart <petestew@gmail.com>
2020-08-29 10:57:03 -0500

    EditBirb working with router

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 class Home extends React.Component {
+  editBirbEvent = (e) => {
+    e.preventDefault();
+    const birbId = 'birb10000';
+    this.props.history.push(`/edit/${birbId}`);
+  };
+
   render() {
     return (
-      <h2>Home</h2>
+      <div className="Home">
+        <h2>Home</h2>
+        <button className="btn btn-danger" onClick={this.editBirbEvent}>Edit A Birb</button>
+      </div>
     );
   }
 }
